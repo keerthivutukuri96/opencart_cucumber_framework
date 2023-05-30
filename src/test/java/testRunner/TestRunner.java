cfe842f0a1d1c099a86d80375c1aa0ba7b4326b1
keerthi vutukuri <keerthivutukuri@gmail.com>
2023-05-30 21:31:16 +0100

    ran using pom.xml

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -10,11 +10,15 @@ import org.junit.runner.RunWith;
         //features= {".//Features//Login.feature"},
         //features= {".//Features/LoginDDT.feature"},
         features= {".//Features/LoginDDTExcel.feature"},
+        //features= {".//Features/LoginDDTExcel.feature",".//Features/LoginDDT.feature"},
+        //features="@target/rerun.txt",  // Runs only failures 
+        
         glue="stepDefinitions",
         plugin= {
         		"pretty", "html:reports/myreport.html", 
         		"json:reports/myreport.json"
-        		},    //Mandatory to capture failures
+        		//"rerun:target/rerun.txt",	//Mandatory to capture failures
+        },
         dryRun=false,
          monochrome=true
         //tags = "@sanity"	//Scenarios tagged with @sanity,
@@ -23,4 +27,3 @@ public class TestRunner
 {
 
 }
-
